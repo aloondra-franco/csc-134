76ae85177802ca1ef03463ff403c606e35f2b06a
aloondra-franco <francoa0951@student.faytechcc.edu>
2026-03-03 02:59:23 +0000

    M2HWQ4 finished

---
+++ M2HWQ4_franco.cpp
@@ -0,0 +1,25 @@
+/*
+CSC 134
+M2HW1
+Alondra Franco
+03/02/2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string school = "FTCC";
+    string team = "Trojans";
+
+    cout << "Welcome all!!";
+    cout << "Here comes FTCC, the Trojans!";
+    
+    cout << "\nLet's go " << school << "!!" << endl;
+    cout << "Let's go " << school << "!!" << endl;
+    cout << "Let's go " << school << "!!" << endl;
+    cout << "Let's go " << team << "!" << endl;
+
+    cout << "Let's have a great time!";
+    return 0
+}
