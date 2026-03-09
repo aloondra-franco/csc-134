bd8404d8adfac99a7c40b9fbe3e33c71ec7567fd
aloondra-franco <francoa0951@student.faytechcc.edu>
2026-03-09 23:54:51 +0000

    M3LAB1 almost finished

---
+++ M3LAB1 _franco.cpp
@@ -0,0 +1,23 @@
+// CSC 134 
+// M3LAB1
+// 03/09/2026
+// Alondra Franco
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// Example #1: single program
+// cout << "Hello World!" << endl; 
+
+cout << "Hello, welcome to Our Bakery! " << endl;
+
+// Example #2: Set a scenario, questions, and answer to them
+// Example test values: 1, 2, 3, banana (try all of them)
+
+// Declare variables
+int choice;
+
+// ask the question
+cout << "At this time the only pastries we are only serving croissants and cinnamon rolls" << endl;
+cout << "Would you like the first or second option?" << endl;
