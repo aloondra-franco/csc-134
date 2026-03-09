38d6c033fdc141dbb423474734ae6a205b78f2f1
aloondra-franco <francoa0951@student.faytechcc.edu>
2026-03-09 21:32:30 +0000

    m3t2 started

---
+++ M3T2_franco.cpp
@@ -0,0 +1,19 @@
+// CSC 134 
+// M3T2
+// 03/09/2026
+// Alondra Franco 
+
+#include <iostream>
+// for pseudo- random numbers
+#include <cmath>
+// for making it truly random
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to Our Casino! "; << endl;
+    cout << "Let's roll some dice!" << endl;
+    int seed = time(0);
+
+    //cout >> 
