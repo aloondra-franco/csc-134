0587c8fcf5f2ed59e092263bf20fe38f10e3d5ad
aloondra-franco <francoa0951@student.faytechcc.edu>
2026-03-17 02:25:42 +0000

    M4T1 finished

--- M4T1_franco.cpp
+++ M4T1_franco.cpp
@@ -9,7 +9,31 @@ using namespace std;
 int main () 
 {
     // Part 1, just say "Hi" five times
-    int count = 1;
-    while (count <= 5) {
-        cout << "Hello number " << count << endl;
+    // Start counting from zero
+    int number = 5;
+
+    int i = 0;
+    while (i < number) {
+        cout << "Hello " << endl;
+        // shorter version
+        i++;
     }
+    cout << "That's all" << endl;
+    
+    
+    // Part 2
+    // Print number
+    const int MIN_NUMBER = 1;
+    const int MAX_NUMBER = 10;
+    
+    int num = MIN_NUMBER;
+    
+    cout << "Number\tNumber Squared" << endl;
+    
+    while (num <== MAX_NUMBER) {
+    cout << num << "\t\t" << num * num << endl;
+    num++;
+    }
+    return 0;
+}
+
