8cecf09ee7410e6f83093643e203cd30779ddd14
aloondra-franco <francoa0951@student.faytechcc.edu>
2026-03-23 21:21:56 +0000

    m4hw1 started

---
+++ M4HW1_franco.cpp
@@ -0,0 +1,23 @@
+/*
+CSC 134
+M$HW1
+Aondra Franco 
+3/23/26
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    // declare variables
+    int numValue;
+
+    // Ask user to input a value
+    cout << "Hello Welcome!" << endl;
+    cout << "Please select a value from 1- 12." << endl;
+    cout << "This value will be put to the multiplication table." << endl;
+    cin >> numValue;
+
+
+
