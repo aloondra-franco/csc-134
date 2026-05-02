a7fd4043db3a859e076b8a5fb9e56f61ded9a9a3
aloondra-franco <francoa0951@student.faytechcc.edu>
2026-05-02 19:10:53 +0000

    M5T2 almost completed

--- M5T2_franco.cpp
+++ M5T2_franco.cpp
@@ -6,4 +6,22 @@
 #include iostream
 using namespace std;
 
-void printResult
+void printResult(int number, int result) {
+    cout << number << " Squared = " << result << endl;
+}
+
+int square(int number) {
+    int result; 
+    result = number * number; // squared
+    return result;
+}
+
+int main() {
+    // count and find results for each one 
+    int count = 1;
+    int result;
+
+    while (count <= 10) {
+        reesult = square(count);
+        printResult
+
