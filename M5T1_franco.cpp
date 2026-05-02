6cfdbc1018d6d46fc61334a9715c0872418c6a04
aloondra-franco <francoa0951@student.faytechcc.edu>
2026-05-02 18:18:06 +0000

    M5T1 almost done

--- M5T1_franco.cpp
+++ M5T1_franco.cpp
@@ -5,3 +5,29 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// Declare Functions
+string formatAnswer(int answer); 
+void printAnswer(string msg);
+
+int main() {
+    int answer = 5;
+    string message; 
+    message = formatAnswer(answer); 
+    printAnswer(message); 
+    return 0;
+}
+
+// Define Functions 
+string formatAnswer (int answer) {
+    // String containing answer
+    string answerMessage; 
+    answerMessage = "The answer is ";
+    answerMessage += to_string(answer);
+    return answerMessage; 
+}
+
+void printAnswer(string msg) {
+    // Display message 
+    cout << msg << endl;
+}
